df0928f938cafee99135f4f48f08af6d3139df27
sarvar-ds0201 <sarvarruzmatov0201@gmail.com>
2026-05-24 20:41:29 +0500

    Create random_walk_visual2.py

---
+++ random_walk_visual2.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Новые блуждания создаются до тех пор, пока программа остается активной.
+while True:
+    # Создание случайного блуждания.
+    rw = RandomWalk(5_000)
+    rw.fill_walk()
+
+    # Нанесение точек на диаграмму.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+    ax.plot(rw.x_values, rw.y_values, linewidth=2)
+    point_numbers = range(rw.num_points)
+    ax.set_aspect('equal')
+
+    # Выделение первой и последней точек.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+               s=100)
+
+    # Удаление осей.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
