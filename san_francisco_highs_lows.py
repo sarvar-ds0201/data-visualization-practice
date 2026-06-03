5e43f5aaa8766ff1b202ece9a5f2fad527607765
sarvar-ds0201 <sarvarruzmatov0201@gmail.com>
2026-06-03 19:01:36 +0500

    Create san_francisco_highs_lows.py

---
+++ san_francisco_highs_lows.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+path = Path('san_francisco_2021.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Извлечение дат и максимальных температур.
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[5])
+        low = int(row[6])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# Создание диаграммы максимальных температур.
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+ax.set_ylim(0, 140)
+
+# Форматирование диаграммы.
+ax.set_title("Daily High and Low Temperatures San-Francisco - 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+plt.show()
